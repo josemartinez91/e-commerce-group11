61d2971d47a9538c5a74c6598baebfc1cb2501a0
Juanery Gonzalez <jdiomarg@gmail.com>
2022-06-11 00:25:02 -0400

    Listado de Telefonos

---
+++ js/stock.js
@@ -0,0 +1,22 @@
+let stockProductos = [
+    { id: 1, marca: "Samsung", modelo: "S21 ultra", Ram: "12 GB", color: "Negro", Cantidad: 1, Precio: 1500 } //Samsungs
+    { id: 2, marca: "Samsung", modelo: "S22 Ultra" Ram: "12 GB", color: "Negro", Cantidad: 1, Precio: 1800 }
+    { id: 3, marca: "Samsung", modelo: "S22 Plus", Ram: "12 GB", color: "Azul", Cantidad: 1, Precio: 1200 }
+    { id: 4, marca: "Samsung", modelo: "Galaxy S21 FE", Ram: "12 GB", color: "Morado", Cantidad: 1, Precio: 1000 }
+    { id: 5, marca: "Iphone", modelo: "13 Pro Max", Ram: "12 GB", color: "Blanco", Cantidad: 1, Precio: 1600 } //Iphones
+    { id: 6, marca: "Iphone", modelo: "13 Pro", Ram: "2 GB", color: "Azul", Cantidad: 1, Precio: 1300 }
+    { id: 7, marca: "Iphone", modelo: "12 Pro", Ram: "2 GB", color: "Rojo", Cantidad: 1, Precio: 1100 }
+    { id: 8, marca: "Iphone", modelo: "13 Mini", Ram: "2 GB", color: "Rosado", Cantidad: 1, Precio: 950 }
+    { id: 9, marca: "Iphone", modelo: "12 Mini", Ram: "2 GB", color: "Negro", Cantidad: 1, Precio: 800 }
+    { id: 10, marca: "Iphone", modelo: "SE 2022", Ram: "2 GB", color: "Rojo", Cantidad: 1, Precio: 600 }
+    { id: 11, marca: "Xiaomi", modelo: "Redmi Note 11 Pro+ 5G", Ram: "12 GB", color: "Negro", Cantidad: 1, Precio: 1100 } //Xiaomi
+    { id: 12, marca: "Xiaomi", modelo: "12 Pro", Ram: "12 GB", color: "Negro", Cantidad: 1, Precio: 1600 }
+    { id: 13, marca: "Xiaomi", modelo: "11T Pro", Ram: "12 GB", color: "Negro", Cantidad: 1, Precio: 1400 }
+    { id: 14, marca: "Xiaomi", modelo: "11 Lite 5G NE", Ram: "12 GB", color: "Negro", Cantidad: 1, Precio: 1300 }
+    { id: 15, marca: "Xiaomi", modelo: "POCO X4 GT Pro", Ram: "8 GB", color: "Azul", Cantidad: 1, Precio: 1200 } //XiaomiPoco
+    { id: 16, marca: "Xiaomi", modelo: "POCO C40+", Ram: "6 GB", color: "Amarillo", Cantidad: 1, Precio: 800 } 
+    { id: 17, marca: "Xiaomi", modelo: "POCO F4 Pro", Ram: "12 GB", color: "Gris", Cantidad: 1, Precio: 1100 }
+    { id: 18, marca: "Sony", modelo: "Xperia Pro-I", Ram: "12 GB", color: "Gris", Cantidad: 1, Precio: 1000 } //Sony
+    { id: 19, marca: "Sony", modelo: "Xperia 10 IV", Ram: "6 GB", color: "Gris", Cantidad: 1, Precio: 900 }
+    { id: 20, marca: "Sony", modelo: "Xperia 1 IV", Ram: "12 GB", color: "Gris", Cantidad: 1, Precio: 1500 }
+]
